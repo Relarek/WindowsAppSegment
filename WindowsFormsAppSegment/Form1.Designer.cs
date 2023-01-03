c14b52ab99b82d53c56a3689a68b99d0dd4d9626
Relarek <117584900+Relarek@users.noreply.github.com>
2023-01-03 22:47:23 +0300

    AppSegment

---
+++ WindowsFormsAppSegment/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsAppSegment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(530, 100);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(460, 440);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 100);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(460, 440);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(34, 30);
+            this.buttonOpen.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(90, 36);
+            this.buttonOpen.TabIndex = 4;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExit.Location = new System.Drawing.Point(900, 580);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(90, 36);
+            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(255)))), ((int)(((byte)(242)))));
+            this.ClientSize = new System.Drawing.Size(1024, 641);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Segmentation";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ WindowsFormsAppSegment/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppSegment
+{
+    public partial class Form1 : Form
+    {
+        public static string fileName;
+        public static string fileNameOut;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            // открытие файла
+            openFileDialog1.Filter = "All files (*.*)|*.*"; //(.bmp)|*.bmp|
+            openFileDialog1.Title = "Open an Image File";
+            openFileDialog1.FileName = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            fileName = openFileDialog1.FileName;
+
+            // загрузка файла в pictureBox1
+            Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+
+            // обработка изображения
+            Recursive_connected_components(bmp);
+        }
+        //
+        private void Recursive_connected_components(Bitmap B)
+        {
+            int width = B.Width;
+            int height = B.Height;
+
+            byte[] inputBytes = MyLoadBMP(B);
+            sbyte[] outputBytes;
+            outputBytes = Negate(inputBytes);
+            int label = 0;
+            Find_components(outputBytes, width, height, label);
+
+            Bitmap bmpOut;
+            if (SaveBMP(outputBytes, width, height) != "")
+            {
+                bmpOut = new Bitmap(fileNameOut);
+                pictureBox2.Image = bmpOut;
+            }
+        }
+
+        public static byte[] MyLoadBMP(Bitmap input)
+        {
+            BitmapData curBitmapData = input.LockBits(new Rectangle(0, 0, input.Width, input.Height), ImageLockMode.ReadOnly, PixelFormat.Format8bppIndexed);
+            int stride = curBitmapData.Stride;
+            byte[] data = new byte[stride * input.Height];
+            Marshal.Copy(curBitmapData.Scan0, data, 0, data.Length);
+            input.UnlockBits(curBitmapData);
+            byte[] outdata = new byte[input.Width * input.Height];
+
+            for (int i = 0; i < input.Height; i++)
+            {
+                //String s = "";
+                for (int j = 0; j < input.Width; j++)
+                {
+                    outdata[j + i * input.Width] = data[i * stride + j];
+                    //s += outdata[j + i * input.Width] == 1 ? ' ' : '*';
+                }
+            }
+            return outdata;
+        }
+        
+        public static sbyte[] Negate(byte[] inb)
+        {
+            int L = inb.Length;
+            sbyte[] outsb = new sbyte[inb.Length];
+
+            for (int i = 0; i < L; i++)
+            {
+                if (inb[i] == 0)
+                {
+                    outsb[i] = -1;
+                }
+                else if (inb[i] == 1)
+                {
+                    outsb[i] = 15;
+                }
+            }
+
+            return outsb;
+        }
+
+        public static void Find_components(sbyte[] LB, int width, int height, int label)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (LB[i * width + j] == -1)
+                    {
+                        label = (label + 1) % 16;
+
+                        Search(LB, label, i, j, width, height);
+                    }
+                }
+            }
+        }
+
+        public static int[,] Neighbors(int r, int c, int width, int height)
+        {
+            int[,] n = new int[4, 2] { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+            кint[,] n = new int[4, 2] { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+
+            if ((r > 0) && (c > 0) && (r < height) && (c < width))
+            {
+                //n = new int[4, 2] { { r - 1, c }, { r, c - 1 }, { r, c + 1 }, { r + 1, c } }; 
+
+                n[0, 0] = r - 1;
+                n[0, 1] = c;
+
+                n[1, 0] = r;
+                n[1, 1] = c - 1;
+
+                n[2, 0] = r;
+                n[2, 1] = c + 1;
+
+                n[3, 0] = r + 1;
+                n[3, 1] = c;
+            }
+
+            return n;
+        }
+
+        public static void Search(sbyte[] LB, int label, int r, int c, int width, int height)
+        {
+            LB[r * width + c] = Convert.ToSByte(label);
+            int[,] Nset = Neighbors(r, c, width, height);
+
+            for (int i = 0; i < Nset.GetLength(0); i++)
+            {
+                if (Nset[i, 0] >= 0 && LB[Nset[i, 0] * width + Nset[i, 1]] == -1)
+                {
+                    Search(LB, label, Nset[i, 0], Nset[i, 1], width, height);
+                }
+            }
+            return;
+        }
+
+        private string SaveBMP(sbyte[] buffer, int width, int height)
+        {
+            //sbyte[] buffer, int width, int height
+            /*
+            for (int i = 0; i < height; i++)
+            {
+                String s = "";
+                for (int j = 0; j < width; j++)
+                    s += buffer[i * width + j] == 15 ? ' ' : Convert.ToChar((buffer[i * width + j] + 33) % 128);
+                Console.WriteLine(s);
+            }
+            */            
+
+            Bitmap b = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+
+            Rectangle BoundsRect = new Rectangle(0, 0, width, height);
+            BitmapData bmpData = b.LockBits(BoundsRect,
+            ImageLockMode.WriteOnly,
+            b.PixelFormat);
+
+            IntPtr ptr = bmpData.Scan0;
+
+            // add back dummy bytes between lines, make each line be a multiple of 4 bytes 
+            int skipByte = bmpData.Stride - width;
+            byte[] newBuff = new byte[buffer.Length + skipByte * height];
+            for (int j = 0; j < height; j++)
+            {
+                Buffer.BlockCopy(buffer, j * (width), newBuff, j * (width + skipByte), width);
+            }
+
+            // fill in rgbValues 
+            Marshal.Copy(newBuff, 0, ptr, newBuff.Length);
+            b.UnlockBits(bmpData);
+
+            // диалог сохранения файла
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.FileName = fileName.Replace(".", "Out.").Replace(".bmp", "");
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)    // если отмена сохранения
+            {
+                return "";
+            }
+
+            fileNameOut = saveFileDialog1.FileName;
+
+            // если имя файла не пусто, сохраняем выходное изображение в файл
+            if (fileNameOut != "")
+            {
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        b.Save(fileNameOut, ImageFormat.Jpeg);
+                        break;
+
+                    case 2:
+                        b.Save(fileNameOut, ImageFormat.Bmp);
+                        break;
+
+                    case 3:
+                        b.Save(fileNameOut, ImageFormat.Gif);
+                        break;
+                }
+            }
+
+            return fileNameOut;
+        }
+    }
+}
